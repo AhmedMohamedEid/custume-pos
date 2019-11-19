c937920a8e0a6dad04bc6eb97a704e7b20c4c410
Ahmed Eid <prog.ahmed.eid@gmail.com>
2019-11-19 15:59:00 +0200

    [MOD] rename Name

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "POS Custome",
+    'name': "POS Custom",
 
     'summary': """
 
--- controllers/controllers.py
+++ controllers/controllers.py
@@ -2,19 +2,19 @@
 from odoo import http
 
 # class CustomePos(http.Controller):
-#     @http.route('/custome_pos/custome_pos/', auth='public')
+#     @http.route('/customize-pos/customize-pos/', auth='public')
 #     def index(self, **kw):
 #         return "Hello, world"
 
-#     @http.route('/custome_pos/custome_pos/objects/', auth='public')
+#     @http.route('/customize-pos/customize-pos/objects/', auth='public')
 #     def list(self, **kw):
 #         return http.request.render('custome_pos.listing', {
-#             'root': '/custome_pos/custome_pos',
-#             'objects': http.request.env['custome_pos.custome_pos'].search([]),
+#             'root': '/customize-pos/customize-pos',
+#             'objects': http.request.env['customize-pos.customize-pos'].search([]),
 #         })
 
-#     @http.route('/custome_pos/custome_pos/objects/<model("custome_pos.custome_pos"):obj>/', auth='public')
+#     @http.route('/customize-pos/customize-pos/objects/<model("customize-pos.customize-pos"):obj>/', auth='public')
 #     def object(self, obj, **kw):
-#         return http.request.render('custome_pos.object', {
+#         return http.request.render('customize-pos.object', {
 #             'object': obj
 #         })
